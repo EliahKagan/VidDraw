b77f997f47ddf9aca107ee7ebcaf9587c30bd4a6
Eliah Kagan <degeneracypressure@gmail.com>
2021-03-17 15:52:51 -0400

    initial commit

---
+++ MainWindow.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace VidDraw {
+    partial class MainWindow {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) {
+                components?.Dispose();
+                timer.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            //
+            // canvas
+            //
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(13, 13);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(800, 600);
+            this.canvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            //
+            // MainWindow
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 626);
+            this.Controls.Add(this.canvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainWindow";
+            this.Text = "VidDraw - Draw to record video";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+    }
+}
+
---
+++ MainWindow.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Timers;
+using System.Windows.Forms;
+using SharpAvi.Output;
+
+namespace VidDraw {
+    public partial class MainWindow : Form {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new(interval: IntervalInMilliseconds) {
+                Enabled = false,
+                SynchronizingObject = this,
+            };
+            timer.Elapsed += timer_Elapsed;
+
+            rectangle = new(Point.Empty, canvas.Size);
+            bitmap = new(width: rectangle.Width, height: rectangle.Height);
+            graphics = Graphics.FromImage(bitmap);
+            graphics.FillRectangle(Brushes.White, rectangle);
+            canvas.Image = bitmap;
+        }
+
+        private const int IntervalInMilliseconds = 30;
+
+        // FIXME: Use a different filename if the file already exists.
+        // Currently, the program crashes when that happens.
+        //
+        // To avoid a race condition, maybe this should open and return a
+        // StreamWriter instead.
+        private static string GetSavePath()
+            => Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyVideos),
+                $"VidDraw capture {DateTime.Now:yyyy-MM-dd HH-mm-ss}.avi");
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            if (stream is null) return;
+
+            var data = new byte[rectangle.Width * rectangle.Height * 4];
+
+            var bits = bitmap.LockBits(rectangle,
+                                       ImageLockMode.ReadOnly,
+                                       PixelFormat.Format32bppRgb);
+            // FIXME: This vertically reflects the image. Don't do that.
+            Marshal.Copy(bits.Scan0, data, 0, data.Length);
+            bitmap.UnlockBits(bits);
+
+            stream.WriteFrame(true, data, 0, data.Length);
+        }
+
+        private void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (rectangle.Contains(e.Location)
+                        && e.Button is MouseButtons.Left) {
+                bitmap.SetPixel(e.Location.X, e.Location.Y, Color.Black);
+                canvas.Invalidate(new Rectangle(e.Location, new Size(1, 1)));
+            }
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button.HasFlag(MouseButtons.Left)) {
+                graphics.DrawLine(pen, oldLocation, e.Location);
+
+                var x1 = Math.Min(oldLocation.X, e.Location.X);
+                var y1 = Math.Min(oldLocation.Y, e.Location.Y);
+                var x2 = Math.Max(oldLocation.X, e.Location.X);
+                var y2 = Math.Max(oldLocation.Y, e.Location.Y);
+
+                var corner = new Point(x: x1, y: y1);
+                var size = new Size(width: x2 - x1 + 1, height: y2 - y1 + 1);
+
+                canvas.Invalidate(new Rectangle(corner, size));
+            }
+
+            oldLocation = e.Location;
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (writer is not null) return;
+
+            writer = new(GetSavePath()) {
+                FramesPerSecond = 1000m / IntervalInMilliseconds,
+                EmitIndex1 = true,
+            };
+
+            Debug.Assert(stream is null);
+            stream = writer.AddVideoStream(width: rectangle.Width,
+                                           height: rectangle.Height);
+
+            BackColor = Color.Red;
+
+            timer.Enabled = true;
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons is not MouseButtons.None || writer is null)
+                return;
+
+            timer.Enabled = false;
+
+            stream = null;
+            // FIXME: After this, it should be possible to play the file. But
+            // for some reason, I can't do that until this program has quit.
+            writer.Close();
+            writer = null;
+
+            BackColor = DefaultBackColor;
+        }
+
+        private readonly System.Timers.Timer timer;
+
+        private readonly Rectangle rectangle;
+
+        private readonly Bitmap bitmap;
+
+        private readonly Graphics graphics;
+
+        private readonly Pen pen = new(Color.Black);
+
+        private Point oldLocation = Point.Empty;
+
+        private AviWriter? writer = null;
+
+        private IAviVideoStream? stream = null;
+    }
+}
