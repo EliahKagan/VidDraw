92682620f2e036ce7a05e15afebc93c32f794242
Eliah Kagan <degeneracypressure@gmail.com>
2021-05-06 13:50:26 -0400

    fixed process enumerate-exit-query race condition

--- ProcessExtensions.cs
+++ ProcessExtensions.cs
@@ -10,8 +10,16 @@ namespace VidDraw {
             try {
                 return process.MainModule?.FileName;
             } catch (Win32Exception) {
-                return null;
+                // This happens when we don't have permission to access the
+                // other process's metadata. VidDraw only queries processes
+                // running in the same session as it, so this is most commonly
+                // because the other process is UAC-elevated (but we are not).
+            } catch (InvalidOperationException) {
+                // This happens if a process exited after being enumerated but
+                // before its metadata are accessed through the Process object.
             }
+
+            return null;
         }
 
         internal static bool HasKnownPath(this Process process,
