EliahKagan/VidDraw

commit 1e16522f76b8fe48fa2fa2b6cbdd6b928f90fbfd

base toast cleanup on UUID identity, not executible path identity

Program.cs
@@ -1,37 +1,20 @@
 using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace VidDraw {
     internal static class Program {
-        static Program()
-        {
-            var path = Me.TryGetPath();
-            Debug.Assert(path is not null);
-            MyPath = path;
-        }
-
-        private static Process Me { get; } = Process.GetCurrentProcess();
-
-        private static string MyPath { get; }
-
-        private static bool InstanceIsUnique
-            => !Process.GetProcesses().Any(p => p.SessionId == Me.SessionId
-                                             && p.HasKnownPath(MyPath)
-                                             && p.Id != Me.Id);
-
         [STAThread]
         private static void Main()
         {
-            using var mutex = Sync.CreateMutex(MyPath);
+            using var locker = Sync.CreateMutex("main");
 
-            // If any VidDraw process may be in the middle of shutting down,
-            // wait for it before proceeding, so that if it uninstalls toasts,
-            // it does so before any of this instance's toasts are dispatched.
-            using (var @lock = new Lock(mutex)) { }
+            Mutex checker;
+            using (var @lock = new Hold(locker))
+                checker = Sync.CreateMutex("checker");
 
             ToastNotificationManagerCompat.OnActivated +=
                 ToastNotificationManagerCompat_OnActivated;
@@ -39,10 +22,11 @@ namespace VidDraw {
             try {
                 Run();
             } finally {
-                using var @lock = new Lock(mutex);
-
-                if (InstanceIsUnique)
-                    ToastNotificationManagerCompat.Uninstall();
+                using var hold = new Hold(locker);
+                checker.Dispose();
+                checker = Sync.CreateMutex("checker", out var createdNew);
+                if (createdNew) ToastNotificationManagerCompat.Uninstall();
+                checker.Dispose(); // Not strictly needed, as the OS cleans up.
             }
         }
 

Config.cs
@@ -18,7 +18,7 @@ namespace VidDraw {
             int[]? CustomColors) {
         internal static Config TryLoad()
         {
-            using var @lock = new Lock(Mutex);
+            using var @lock = new Hold(Mutex);
             return TryRead();
         }
 
@@ -26,7 +26,7 @@ namespace VidDraw {
 
         internal void TrySave()
         {
-            using var @lock = new Lock(Mutex);
+            using var @lock = new Hold(Mutex);
             TryRead().PatchedBy(this).TryWrite();
         }
 

Hold.cs
@@ -6,7 +6,7 @@ namespace VidDraw {
     /// Acquiring the mutex due to another thread/process abandoning it is not
     /// conisdered an error.
     /// </remarks>
-    internal readonly ref struct Lock {
+    internal readonly ref struct Hold {
         /// <summary>Acquires the mutex, waiting if necessary.</summary>
         /// <remarks>
         /// Like <see cref="WaitHandle.WaitOne()"/> but also returns (rather
@@ -15,7 +15,7 @@ namespace VidDraw {
         /// held it.
         /// </remarks>
         /// <param name="mutex">The mutex to acquire.</param>
-        internal Lock(Mutex mutex)
+        internal Hold(Mutex mutex)
         {
             try {
                 mutex.WaitOne();

ProcessExtensions.cs
@@ -1,29 +1,0 @@
-using System;
-using System.ComponentModel;
-using System.Diagnostics;
-
-namespace VidDraw {
-    /// <summary>Extensions for comparing processes.</summary>
-    internal static class ProcessExtensions {
-        internal static string? TryGetPath(this Process process)
-        {
-            try {
-                return process.MainModule?.FileName;
-            } catch (Win32Exception) {
-                // This happens when we don't have permission to access the
-                // other process's metadata. VidDraw only queries processes
-                // running in the same session as it, so this is most commonly
-                // because the other process is UAC-elevated (but we are not).
-            } catch (InvalidOperationException) {
-                // This happens if a process exited after being enumerated but
-                // before its metadata are accessed through the Process object.
-            }
-
-            return null;
-        }
-
-        internal static bool HasKnownPath(this Process process,
-                                          string fileName)
-            => fileName.Equals(process.TryGetPath(), StringComparison.Ordinal);
-    }
-}

Sync.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 
 namespace VidDraw {
     /// <summary>
@@ -9,12 +6,11 @@ namespace VidDraw {
     /// </summary>
     internal static class Sync {
         internal static Mutex CreateMutex(string label)
-            => new(initiallyOwned: false, $"{Uuid}-{Hash(label)}");
+            => new(initiallyOwned: false, $"{Uuid}-{label}");
+
+        internal static Mutex CreateMutex(string label, out bool createdNew)
+            => new(initiallyOwned: false, $"{Uuid}-{label}", out createdNew);
 
         private const string Uuid = "e3f36cbf-64b5-4cd8-b334-f24bf69a65c9";
-
-        private static string Hash(string label)
-            => string.Concat(SHA256.HashData(Encoding.UTF8.GetBytes(label))
-                                   .Select(octet => octet.ToString("x2")));
     }
 }

commit 0906a71ddfc30d5f9edf92089f6617ca013456ab

synchronize toast cleanup per-architecture; show architecture in title bar
This fixes a regression introduced in 1e16522 where running 32- and
64-bit builds at the same time could cause toasts from one not to
be cleaned up.

Now, toasts *for an architecture* are cleaned up when an exiting
VidDraw process *of that architecture* detects that it is the last
VidDraw process *of that architecture*.

This could give nonintuitive results, if a user runs two or more
VidDraw processes of one architecture and one or more VidDraw
processes of the other architecture at the same time and doesn't
recognize the architecture distinction as important. So I've also
added the architecture to the main window's title bar text. This is
really something I should've done anyway, ever since using a codec
(x264vfw) that can be installed for one architecture and not the
other (and that is separately confgured for each architecture).

I'm still synchronizing the checks with the same mutex, which means
there remains a strange dependency between x64 and x86 VidDraw
processes. This is intentional, because I'm not sure if I'm going
to keep doing cleanup in this somewhat undesirable per-architecture
way. If I don't, I may be glad, for the purpose of compatibility
between older and newer versions of VidDraw, that I kept the "main"
mutex architecture-neutral (and kept its name the same).

MainWindow.Designer.cs
@@ -50,7 +50,7 @@ namespace VidDraw {
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainWindow";
-            this.Text = "VidDraw - Draw to record video";
+            this.Text = "VidDraw (error)";
             ((System.ComponentModel.ISupportInitialize)(this._canvas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();

MainWindow.cs
@@ -25,6 +25,7 @@ namespace VidDraw {
             _pen = new(_colorPicker.Color);
             _recorder = new(_bitmap, this);
 
+            SetInitialTitle();
             _recorder.Recorded += recorder_Recorded;
             ClearCanvas();
         }
@@ -204,6 +205,14 @@ namespace VidDraw {
                 .Show();
         }
 
+        private void SetTitle(string message)
+            => Text = $"VidDraw ({Program.XStyleArch}) - {message}";
+
+        private void SetInitialTitle() => SetTitle("Draw to record video");
+
+        private HMENU MenuHandle
+            => PInvoke.GetSystemMenu(hWnd: new(Handle), bRevert: false);
+
         private Codec CurrentCodec
         {
             get => CodecChoices.Single(choice => HasCheck(choice.Id)).Codec;
@@ -215,9 +224,6 @@ namespace VidDraw {
                 Debug.Assert(CurrentCodec == value);
             }
         }
-
-        private HMENU MenuHandle
-            => PInvoke.GetSystemMenu(hWnd: new(Handle), bRevert: false);
 
         private void AddMenuSeparator()
             => PInvoke.AppendMenu(hMenu: MenuHandle,
@@ -324,9 +330,8 @@ namespace VidDraw {
         {
             if (CanEncodeH264) {
                 SetEnabled(MyMenuItemId.H264, true);
-                var arch = (Environment.Is64BitProcess ? "x64" : "x86");
                 SetText(MyMenuItemId.DownloadOrConfigureX264vfw,
-                        $"Configure x264vfw ({arch})");
+                        $"Configure x264vfw ({Program.XStyleArch})");
                 _downloadOrConfigureX264vfw = ConfigureX264vfw;
             } else {
                 if (CurrentCodec is Codec.H264)
@@ -381,7 +386,7 @@ namespace VidDraw {
 
             UpdateMenuCodecs();
 
-            Text = $"VidDraw - Recording{Ch.Hellip}";
+            SetTitle($"Recording{Ch.Hellip}");
             BackColor = Color.Red;
 
             _recorder.Start(Files.CreateWithoutClash(CurrentPreferredSavePath),
@@ -396,7 +401,7 @@ namespace VidDraw {
             _recorder.Finish();
 
             BackColor = DefaultBackColor;
-            Text = "VidDraw - Draw to record video";
+            SetInitialTitle();
         }
 
         private void SelectCodec(Codec codec)

Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace VidDraw {
     internal static class Program {
+        internal static string XStyleArch
+            => Environment.Is64BitProcess ? "x64" : "x86";
+
         [STAThread]
         private static void Main()
         {
@@ -14,7 +17,7 @@ namespace VidDraw {
 
             Mutex checker;
             using (var @lock = new Hold(locker))
-                checker = Sync.CreateMutex("checker");
+                checker = Sync.CreateMutex($"checker-{XStyleArch}");
 
             ToastNotificationManagerCompat.OnActivated +=
                 ToastNotificationManagerCompat_OnActivated;